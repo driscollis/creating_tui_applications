52cfdabea691dca81fc6ddca05a154658ec95f88
Mike Driscoll <mike@pythonlibrary.org>
2025-04-09 08:13:43 -0500

    Add chapter 8 code

---
+++ 08_key/bindings.py
@@ -0,0 +1,30 @@
+# bindings.py
+
+from textual.app import App, ComposeResult
+from textual.color import Color
+from textual.widgets import Footer, Button
+
+
+class BindingApp(App):
+    BINDINGS = [
+        ("g", "add_button('green')", "Add green button"),
+        ("y", "add_button('yellow')", "Add yellow button"),
+        ("escape", "esc", "Exit application")
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Footer()
+
+    def action_add_button(self, color: str) -> None:
+        button = Button(color)
+        button.styles.background = Color.parse(color)
+        self.mount(button)
+        self.call_after_refresh(self.screen.scroll_end, animate=False)
+
+    def action_esc(self) -> None:
+        self.exit()
+
+
+if __name__ == "__main__":
+    app = BindingApp()
+    app.run()
