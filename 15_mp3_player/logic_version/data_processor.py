93cbd845c8d1ad2dc6a8cf84dfb359ad1b595ba3
Mike Driscoll <mike@pythonlibrary.org>
2025-04-09 08:15:10 -0500

    Add chapter 15 code

---
+++ 15_mp3_player/logic_version/data_processor.py
@@ -0,0 +1,30 @@
+# data_processor.py
+
+import eyed3
+import pathlib
+
+
+def get_mp3(path: pathlib.Path) -> eyed3.AudioFile | None:
+    mp3 = eyed3.load(str(path))
+    track_num = mp3.tag.track_num.count
+    title = mp3.tag.title
+    length = f"{int(mp3.info.time_secs // 60)}:{int(mp3.info.time_secs % 60):02d}"
+    artist = mp3.tag.artist
+    album = mp3.tag.album
+    return album, track_num, title, length, artist, str(path)
+
+
+def process_mp3s(path: pathlib.Path) -> list[str]:
+    """
+    Process the MP3 file or folder and turn it into a list of lists
+    """
+    mp3s = []
+
+    mp3s.append(["Album", "#", "Title", "Length", "Artist", "Location"])
+
+    if path.is_file():
+        mp3s.append(list(get_mp3(path)))
+    elif path.is_dir():
+        for mp3_path in path.glob("*.mp3"):
+            mp3s.append(list(get_mp3(mp3_path)))
+    return mp3s
---
+++ 15_mp3_player/mp3_player_poc.py
@@ -0,0 +1,36 @@
+# mp3_player_poc.py
+
+import vlc
+
+from textual import on
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal
+from textual.widgets import Button
+
+
+class MP3Player(App):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.player = None
+        self.current_track = r"PATH TO MP3 FILE"
+
+    def compose(self) -> ComposeResult:
+        yield Horizontal(
+            Button("Play", id="play", variant="primary"),
+            Button("Stop", id="stop", variant="error")
+        )
+
+    @on(Button.Pressed, "#play")
+    def on_play(self) -> None:
+        self.player = vlc.MediaPlayer(self.current_track)
+        self.player.play()
+
+    @on(Button.Pressed, "#stop")
+    def on_stop(self) -> None:
+        self.player.stop()
+
+
+if __name__ == "__main__":
+    app = MP3Player()
+    app.run()
