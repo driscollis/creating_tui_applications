9e71cea1c02cf3d91c27a41a28784501dcb2a6f8
Mike Driscoll <mike@pythonlibrary.org>
2025-04-09 08:13:07 -0500

    Add chapter 5 code

---
+++ 05_query/query_buttons.py
@@ -0,0 +1,26 @@
+# query_buttons.py
+
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Label
+
+
+class QueryApp(App):
+
+    def compose(self) -> ComposeResult:
+        yield Label("Press a button", id="label")
+        yield Button("One", id="one")
+        yield Button("Two", id="two")
+        yield Button("Three")
+
+    def on_button_pressed(self) -> None:
+        widgets = self.query("Button")
+        s = ""
+        for widget in widgets:
+            s += f"{widget}\n"
+        label = self.query_one("#label")
+        label.update(s)
+
+
+if __name__ == "__main__":
+    app = QueryApp()
+    app.run()
